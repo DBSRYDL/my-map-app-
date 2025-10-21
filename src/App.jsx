6288bea364a1702cd57e8e6b2f9ebcfac568735b
DBSRYDL <gyo3065@gmail.com>
2025-10-21 17:49:08 +0900

    V1. 기본적인 지도 노출

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function App() {
+    return (
+        <MapContainer
+            center={[37.5665, 126.9780]} // 서울 시청 좌표
+            zoom={13}
+            style={{ height: "100vh", width: "100%" }}
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; OpenStreetMap contributors"
+            />
+        </MapContainer>
+    );
+}
